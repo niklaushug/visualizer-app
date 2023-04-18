27b6f602bbfef25ba03fe4aace0b4b371c202d0c
Niklaus Hug <niklaus.hug@inventage.com>
2023-04-18 14:04:07 +0200

    Establish script to watermark images.

---
+++ nodejs-only/index.js
@@ -0,0 +1,9 @@
+import {Watermark} from "./mark.js";
+
+const watermark = new Watermark()
+
+watermark.resize('assets/photos/17.jpg', 'assets/logos/inventage-bildmarke-farbe.png');
+watermark.resize('assets/photos/2.jpeg', 'assets/logos/inventage-logo-weiss.png');
+
+watermark.resize('assets/photos/2.jpeg', 'assets/logos/inventage-bildmarke-farbe.png');
+watermark.resize('assets/photos/17.jpg', 'assets/logos/inventage-logo-weiss.png');
---
+++ nodejs-only/mark.js
@@ -0,0 +1,39 @@
+// const Jimp = require('jimp');
+
+import Jimp from 'jimp'
+
+export class Watermark {
+  static OFFSET = 100
+
+  static position(image, watermark) {
+    const x = (image.bitmap.width - watermark.bitmap.width) / 2
+    const y = (image.bitmap.height - watermark.bitmap.height) / 2
+    return [x, y]
+  }
+
+  static sizeOfWatermark(image) {
+    const width = image.bitmap.width - Watermark.OFFSET * 2
+    const height = image.bitmap.height - Watermark.OFFSET * 2
+    return [width, height]
+  }
+
+  async resize(imagePath, watermarkPath) {
+    const startTime = new Date()
+
+    const image = await Jimp.read(imagePath);
+    const watermark = await Jimp.read(watermarkPath);
+
+    await watermark.contain(...Watermark.sizeOfWatermark(image)).autocrop()
+
+    await image.composite(watermark, ...Watermark.position(image, watermark), {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacityDest: 1,
+      opacitySource: 0.5
+    })
+
+    await image.writeAsync(`out/${Date.now()}.png`);
+
+    const endTime = new Date()
+    console.log(`mark "${imagePath}" with "${watermarkPath}" (${endTime - startTime}ms)`)
+  }
+}
